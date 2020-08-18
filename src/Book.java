3694cc9ef4119f62b6825f196ebad09ecfdd5abd
im-yoursaviour <akowuahnimoh@gmail.com>
2020-08-18 11:58:02 +0100

    first commit

---
+++ src/Book.java
@@ -0,0 +1,75 @@
+public class Book {
+    //The fields
+    private String author;
+    private String title;
+    private int pages;
+    private String refNumber="";
+    private boolean onLoan;
+    private int loanCount;
+    /** Setting constructor
+     *
+     */
+    public Book(String author, String title, int pages){
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        onLoan = false;
+        loanCount = 0;
+
+    }
+    //methods come here
+
+    public String getAuthor() {
+        return author;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void printDetails(){
+        if(refNumber==""){
+        System.out.println("Title: " + title + " The author is: " + author + " The number of pages is: "+ pages +
+                "and the book reference is zzzz.");
+    }else {
+            System.out.println("Title: " + title + " The author is: " + author + " The number of pages is: "+ pages +
+                    "and the book reference is: "+ refNumber);
+        }
+    }
+    public void setRefNumber(String refNumber){
+        if(refNumber.length() >= 3) {
+            this.refNumber = refNumber;
+        }else {
+            System.out.println("inappropriate input");
+        }
+    }
+    public String getRefNumber(){
+        return refNumber;
+    }
+
+    public boolean getOnloan(){
+        return onLoan;
+
+    }
+    public int getLoanCount(){
+        return loanCount;
+    }
+
+    public void borrow(){
+        if(onLoan == false){
+            onLoan = true;
+            loanCount++;
+        }else{
+            System.out.println("The book is on loan");
+        }
+
+    }
+
+    public void returnBook(){
+        if(onLoan == false ){
+            System.out.println("the book is not on loan");
+        }else{
+            onLoan = false;
+        }
+    }
+
+
+}
---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args){
+        Book loki = new Book("lalay", "loki the lion king", 35);
+        System.out.println(loki.getAuthor());
+        loki.printDetails();
+    }
+}
